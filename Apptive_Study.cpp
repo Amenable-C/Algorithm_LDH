5b7f3d0778e77dbbaff1568af1ca592d9071c94f
Amenable <duswo0624@naver.com>
2022-04-01 21:24:08 +0900

    boj1158

---
+++ Apptive_Study.cpp
@@ -0,0 +1,32 @@
+﻿#include <iostream>
+#include <queue>
+using namespace std;
+
+int main()
+{
+    int n, k;
+    cin >> n >> k;
+
+    queue<int> q;
+    for (int i = 1; i < n + 1; i++)
+        q.push(i);
+    
+    cout << "<";
+    while (q.size() > 0) {
+        if (q.size() == 1) {
+            cout << q.front() << ">";
+            break;
+        }
+
+        for (int i = 0; i < k - 1; i++) {
+            q.push(q.front());
+            q.pop();
+        }
+
+        cout << q.front() << ", ";
+        q.pop();
+    }
+    return 0;
+}
+
+
